4e40cd6293479641838352376134164f51c5c9d0
Kanon35s <rashidmostafa001@gmail.com>
2026-02-06 22:08:22 +0600

    latest update on backend completion
    backend complete!!

--- railbite-backend/routes/adminReportRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/adminReportRoutes.js
-const express = require('express');
-const router = express.Router();
-const { protect, admin } = require('../middleware/auth');
-const { getAdminReports } = require('../controllers/adminReportController');
-
-router.get('/admin/reports', protect, admin, getAdminReports);
-
-module.exports = router;
