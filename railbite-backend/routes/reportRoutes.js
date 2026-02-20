bfdc982ecc0c3009b0b381573ea1ba762c1d0092
Kanon35s <rashidmostafa001@gmail.com>
2026-02-20 18:51:43 +0600

    backend correction
    backend correction

--- railbite-backend/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getReport, getPopularItems } = require('../controllers/reportController');
-const { protect, admin } = require('../middleware/auth');
-
-router.get('/', protect, admin, getReport);
-router.get('/popular-items', protect, admin, getPopularItems);
-
-module.exports = router;
