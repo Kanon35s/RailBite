18908aa32b0ed8f3db01a280c1d042958a3fc47c
JAKARIA8785 <ocjakaria8785@gmail.com>
2026-02-21 22:08:51 +0600

    server fix

--- railbite-backend/server.js
+++ railbite-backend/server.js
@@ -1,7 +1,7 @@
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
 dns.setDefaultResultOrder('ipv4first');
-const path = require('path');
+//const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
